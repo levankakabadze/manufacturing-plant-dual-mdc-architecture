88c15275c7ccd23159b75a1357a1adea0d448185
Levan Kakabadze <leokakabadze@gmail.com>
2026-06-15 13:00:26 +0200

    terraform: add VM template data source, resource pool, and VM resources

--- terraform/vsphere/data.tf
+++ terraform/vsphere/data.tf
@@ -20,3 +20,15 @@ data "vsphere_network" "network" {
     name          = var.network
     datacenter_id = data.vsphere_datacenter.dc.id
 }
+
+# Look up the VM template
+data "vsphere_virtual_machine" "template" {
+  name          = "ubuntu-22-04-template"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# Look up the default resource pool for the cluster
+data "vsphere_resource_pool" "pool" {
+    name          = "$var{var.cluster}/Resources"
+    datacenter_id = data.vsphere_datacenter.dc.id
+}
---
+++ terraform/vsphere/main_vm.tf
@@ -0,0 +1,25 @@
+resource "vsphere_virtual_machine" "vm" {
+  name             = "TRGLKVM0001"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 4
+  memory   = 8192
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+  }
+}
